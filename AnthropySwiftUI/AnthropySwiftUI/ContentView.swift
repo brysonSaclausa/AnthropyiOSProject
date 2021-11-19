a95f0c8b608965f2d7c23c3284559d2988d277c4
brysonS <brys1saclaus2@gmail.com>
2021-11-19 14:41:46 -0500

    UI: Sign in view, Sign in with Apple button

--- AnthropySwiftUI/AnthropySwiftUI/ContentView.swift
+++ AnthropySwiftUI/AnthropySwiftUI/ContentView.swift
@@ -4,13 +4,25 @@
 //
 //  Created by BrysonSaclausa on 11/19/21.
 //
-
+import AuthenticationServices
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                SignInWithAppleButton(.continue) { request in
+                    //
+                } onCompletion: { result in
+                    //
+                }
+                .frame(height: 50)
+                .padding()
+                .cornerRadius(8)
+
+            }
+            .navigationTitle("Sign In")
+        }
     }
 }
 
