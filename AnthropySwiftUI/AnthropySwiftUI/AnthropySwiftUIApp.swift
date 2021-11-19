4edd75980fc4a8c9fb4eef925244c80164a029dd
brysonS <brys1saclaus2@gmail.com>
2021-11-19 14:26:02 -0500

    Xcode project init

---
+++ AnthropySwiftUI/AnthropySwiftUI/AnthropySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnthropySwiftUIApp.swift
+//  AnthropySwiftUI
+//
+//  Created by BrysonSaclausa on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct AnthropySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
