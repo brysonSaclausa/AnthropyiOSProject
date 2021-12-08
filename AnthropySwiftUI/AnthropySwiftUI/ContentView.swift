da0fc90d22adddec1f2af1b7158e676181df84e8
brysonS <brys1saclaus2@gmail.com>
2021-12-07 23:41:54 -0500

    Apple sign in work in progress

--- AnthropySwiftUI/AnthropySwiftUI/ContentView.swift
+++ AnthropySwiftUI/AnthropySwiftUI/ContentView.swift
@@ -8,17 +8,57 @@ import AuthenticationServices
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userId") var userId: String = ""
+    
     var body: some View {
         NavigationView {
             VStack {
-                SignInWithAppleButton(.continue) { request in
-                    //
-                } onCompletion: { result in
-                    //
+                
+                if userId.isEmpty {
+                    SignInWithAppleButton(.signIn) { request in
+                        request.requestedScopes = [.email, .fullName]
+                        
+                    } onCompletion: { result in
+                        switch result {
+                        case .success(let auth):
+                            
+                            switch auth.credential {
+                            case let credential as ASAuthorizationAppleIDCredential:
+                                // User ID
+                                let userId = credential.user
+                                // User Info
+                                let email = credential.email
+                                let firstName = credential.fullName?.givenName
+                                let lastName = credential.fullName?.familyName
+                                
+                                self.email = email ?? ""
+                                self.firstName = firstName ?? ""
+                                self.lastName = lastName ?? ""
+                                self.userId = userId
+                                
+                                print("signed in")
+                                
+                            default: break
+                            }
+                        case .failure(let error):
+                            print(error)
+                        }//
+                        
+                    }
+                    .frame(height: 50)
+                    .padding()
+                    .cornerRadius(8)
                 }
-                .frame(height: 50)
-                .padding()
-                .cornerRadius(8)
+                else {
+                    //signed in show webapp
+                    InitialViewTest()
+                }
+                
+                
 
             }
             .navigationTitle("Sign In")
---
+++ AnthropySwiftUI/AnthropySwiftUI/InitialViewTest.swift
@@ -0,0 +1,20 @@
+//
+//  InitialViewTest.swift
+//  AnthropySwiftUI
+//
+//  Created by BrysonSaclausa on 11/19/21.
+//
+
+import SwiftUI
+
+struct InitialViewTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InitialViewTest_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialViewTest()
+    }
+}
